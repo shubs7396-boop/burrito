44d96414078b3aa889e2f408ce96e2fbae6c8112
shubs7396-boop <shubs7396@gmail.com>
2026-03-16 17:14:31 +0530

    Add fallback route to App.jsx to handle undefined paths

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,7 @@ function App() {
             <Route path="/menu" element={<MenuPage />} />
             <Route path="/location" element={<Location />} />
             <Route path="/contact" element={<Contact />} />
+            <Route path="*" element={<Home />} />
           </Routes>
         </main>
         <Footer />
